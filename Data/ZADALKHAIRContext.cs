270fabb23b22d321895144c7787b91fcdea3825a
Zaid El-Atrash <zaidelatrash4@gmail.com>
2021-10-01 06:14:03 +0300

    New Update

---
+++ Data/ZADALKHAIRContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ZADALKHAIR.Models;
+
+namespace ZADALKHAIR.Data
+{
+    public class ZADALKHAIRContext : DbContext
+    {
+        public ZADALKHAIRContext (DbContextOptions<ZADALKHAIRContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ZADALKHAIR.Models.Contact> Contact { get; set; }
+    }
+}
---
+++ Models/Contact.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZADALKHAIR.Models
+{
+    public class Contact
+    {
+        [Required]
+        [Key]
+        public int ContactID { get; set; }
+        [EmailAddress]
+        [Required]
+        public String ContactEmail { get; set; }
+        [Required]
+        [StringLength(maximumLength: 255)]
+        public String ContactName { get; set; }
+        [Required]
+        [StringLength(maximumLength: 10)]
+        public String ContactPhoneNumber { get; set; }
+        [Required]
+        [StringLength(maximumLength: 255)]
+        public String ContactCounrty { get; set; }
+        [Required]
+        public String ContactSubject { get; set; }
+        [Required]
+        [StringLength(maximumLength: 765)]
+        public String ContactMassege { get; set; }
+        public bool ContactSatuts { get; set; } = false;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime SatutsUpdate { get; set; }
+    }
+}
