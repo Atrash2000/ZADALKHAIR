35455d7215a90986cf743e231dca8dca6498955b
Ibraheem Fatayer <ibraheem.7azem@gmail.com>
2021-10-01 19:15:59 +0300

    first commit

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,64 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+const counters = document.querySelectorAll("#counter");
+const speed = 10;
+
+counters.forEach(
+    (counter) => {
+        const updateCount = () => {
+            const target = +counter.getAttribute('data-target');
+            const count = +counter.innerHTML;
+
+            const inc = target / speed;
+
+            /*console.log(inc);*/
+
+            if (count < target) {
+                counter.innerHTML = count + inc;
+                setTimeout(updateCount, 100);
+            }
+            else {
+                count.innerHTML = target;
+            }
+        }
+        updateCount();
+    }
+);
+
+var PrevScroll = 0;
+const nav = document.querySelector("#nav-container");
+const logoImg = document.querySelector("#logo");
+
+function controllNavBar() {
+    var currentScroll = pageYOffset;
+    if (PrevScroll < currentScroll) {
+        nav.classList.add("nav-bg");
+        logoImg.classList.remove("logo-img-1");
+        logoImg.classList.add("logo-img-2");
+    } else {
+        nav.classList.remove("nav-bg");
+        logoImg.classList.remove("logo-img-2");
+        logoImg.classList.add("logo-img-1");
+        currentScroll = PrevScroll;
+    }
+}
+
+/* SideBar */
+const sidebarBg = document.querySelector(".sidebar-bg");
+const sidebar = document.querySelector("aside");
+const btnClose = document.querySelector(".btn-close");
+
+function showSideBar() {
+    sidebarBg.classList.add("d-block");
+    sidebar.classList.add("display-sidebar");
+    btnClose.classList.add("display-close-btn");
+}
+
+function closeSidebar() {
+    sidebarBg.classList.remove("d-block");
+    sidebar.classList.remove("display-sidebar");
+    sidebar.style.transition = "right 1s";
+    btnClose.classList.remove("display-close-btn");
+}
