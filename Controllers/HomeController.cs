0362a98b81e8d79efed0e69c3298ef9d99ca7eba
Ibraheem Fatayer <ibraheem.7azem@gmail.com>
2021-10-01 19:27:46 +0300

    add AboutUs Action method

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -33,5 +33,10 @@ namespace ZADALKHAIR.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
     }
 }
